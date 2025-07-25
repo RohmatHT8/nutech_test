04e364331c99bcf832dd80397da8bbb7210dbc5c
RohmatHT8 <rohmathidayattullah97@gmail.com>
2025-07-25 12:26:18 +0700

    change model

--- config/config.js
+++ config/config.js
@@ -8,5 +8,13 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql'
   }
 };
